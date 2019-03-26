fa2be6d8a3576592f146d74924dd734329cfb644
wangyuxin11 <39932782+wangyuxin11@users.noreply.github.com>
2019-03-26 16:42:36 +0800

    Update AppConfig.scala

--- src/main/scala/com/springboot/in/action/AppConfig.scala
+++ src/main/scala/com/springboot/in/action/AppConfig.scala
@@ -2,5 +2,6 @@ package com.springboot.in.action
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
+//https://www.jianshu.com/p/51535e85bae5
 @SpringBootApplication
 class AppConfig
